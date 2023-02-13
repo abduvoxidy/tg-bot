a9b9475bbeac9ed8db3e0fe6cc53f9960efd06de
abduvoxid <abduvoxidyuldashev@gmail.com>
2023-02-13 12:00:01 +0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import { Main } from 'components/UI/Main/Main'
+import SEO from 'components/SEO'
+import { fetchMultipleUrls } from 'services/fetchMultipleUrls'
+
+export default function Home({ data }) {
+  return (
+    <>
+      <SEO />
+      <Main />
+    </>
+  )
+}
+
+// export async function getServerSideProps(context) {
+//   const urls = ['/posts']
+//   const data = await fetchMultipleUrls(urls)
+
+//   return {
+//     props: {
+//       data
+//     }
+//   }
+// }
---
+++ src/services/posts.js
@@ -0,0 +1,19 @@
+import { useMutation, useQuery } from 'react-query'
+import { request } from './http-client'
+
+const getPosts = (params) => request.get('/posts', { params })
+const createPost = (data) => request.post('/posts', data)
+const updatePost = (id, data) => request.put(`/posts/${id}`, data)
+const deletePost = (id) => request.delete(`/posts/${id}`)
+
+export const usePost = ({ params }) => {
+  const posts = useQuery(['GET_POSTS'], () => getPosts(params), {
+    enabled: true
+  })
+  const createMutation = useMutation(createPost)
+
+  return {
+    posts,
+    createMutation
+  }
+}
