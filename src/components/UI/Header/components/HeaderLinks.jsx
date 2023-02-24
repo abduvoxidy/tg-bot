bd8d81cf92f815c17fa3f06e73f641d9d631a370
abduvoxid <abduvoxidyuldashev@gmail.com>
2023-02-24 18:53:16 +0500

    fix cart

--- src/components/UI/Header/components/HeaderLinks.jsx
+++ src/components/UI/Header/components/HeaderLinks.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import cls from "./styles.module.scss";
 import { CompareIcon, HeartIcon, BasketIcon } from "components/UI/Icons";
+import Link from "next/link";
 
 function HeaderLinks() {
   const icons = [
@@ -17,16 +18,20 @@ function HeaderLinks() {
     {
       icon: <BasketIcon />,
       title: "Корзина",
-      link: "/",
+      link: "/cart",
     },
   ];
   return (
     <div className={cls.HeaderLinks}>
       {icons.map((el, index) => (
-        <div key={index} className={cls.link}>
-          <span>{el.icon}</span>
-          <p>{el.title}</p>
-        </div>
+        <Link href={el.link} key={index}>
+          <a>
+            <div className={cls.link}>
+              <span>{el.icon}</span>
+              <p>{el.title}</p>
+            </div>
+          </a>
+        </Link>
       ))}
     </div>
   );
