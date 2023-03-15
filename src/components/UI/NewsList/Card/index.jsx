92b8534e21561fdbf4042b9f52958c48a2b71bd7
abduvoxid <abduvoxidyuldashev@gmail.com>
2023-03-15 19:15:58 +0500

    news page done

--- src/components/UI/NewsList/Card/index.jsx
+++ src/components/UI/NewsList/Card/index.jsx
@@ -2,26 +2,34 @@ import React from "react";
 import cls from "../NewsList.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import useKeyTranslation from "hooks/useKeyTranslation";
+import format from "date-fns/format";
 
 function Card({ data }) {
+  const getKey = useKeyTranslation();
+  const day = new Date(data?.date);
+  const formatDay = format(day, "dd.MM.yyyy");
   return (
-    <Link href="/">
+    <Link href={`/news/${data.guid}`}>
       <a>
         <div className={cls.card}>
           <div className={cls.img}>
             <Image
               objectFit="cover"
               layout="fill"
-              src={`/images/main/${data.url}`}
-              alt={data.url}
+              src={data.photo}
+              alt={data?.[getKey("name")]}
             />
           </div>
           <div className={cls.cardBody}>
-            <h3 className={cls.title}>
-              {"Отдел радиологии работает круглосуточно"}
-            </h3>
-            <p className={cls.desc}>{data.name}</p>
-            <p className={cls.date}>01.03.2023</p>
+            <h3 className={cls.title}>{data?.[getKey("name")]}</h3>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: data?.[getKey("content")],
+              }}
+              className={cls.desc}
+            />
+            <p className={cls.date}>{formatDay || ""}</p>
           </div>
         </div>
       </a>
--- src/components/UI/NewsList/index.jsx
+++ src/components/UI/NewsList/index.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 import { useNewsQuery } from "services/news.service";
 
 function NewsList() {
-  const { data } = useNewsQuery({
+  const { data: news } = useNewsQuery({
     data: {
       data: {
         offset: 0,
@@ -12,29 +12,15 @@ function NewsList() {
     },
   });
 
-  console.log("data", data);
-
-  const news = [
-    {
-      url: "iwatch.png",
-      name: "Apple Watch получат неинвазивный глюкометр",
-    },
-    {
-      url: "chip.png",
-      name: "Windows 10 станет возможным запускать на Apple M1 Mac",
-    },
-    {
-      url: "airpods.png",
-      name: "AirPods – Стоит ли покупать AirPods 1 в 2021 году?",
-    },
-  ];
   return (
     <div className={cls.root}>
       <h1>Новости</h1>
       <div className={cls.row}>
-        {news.map((el, index) => (
-          <Card data={el} key={index + "news"} />
-        ))}
+        {news
+          ? news.data.response.map((el, index) => (
+              <Card data={el} key={el.guid} />
+            ))
+          : null}
       </div>
     </div>
   );
