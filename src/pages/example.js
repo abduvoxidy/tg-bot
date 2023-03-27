09dcba07a5c340353a20370156805b7304226f05
jamshidev404 <jamshidbek536@gmail.com>
2023-03-27 12:10:53 +0500

    changes

---
+++ src/pages/example.js
@@ -0,0 +1,11 @@
+import SEO from "components/SEO";
+import Example from "components/UI/Example";
+
+export default function brand() {
+  return (
+    <>
+      <SEO />
+      <Example />
+    </>
+  );
+}
