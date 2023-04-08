89d1eb61dcc690ee416e7b55d57245f5ab046175
jamshidev404 <jamshidbek536@gmail.com>
2023-04-08 15:06:38 +0500

    user profile: change ui

--- src/components/UI/User/Profile/PersonalData/PersonalData.jsx
+++ src/components/UI/User/Profile/PersonalData/PersonalData.jsx
@@ -11,6 +11,7 @@ import { BlueTelegramIcon, GmailIcon } from "components/UI/Icons";
 function PersonalData() {
   const [open, setOpen] = useState(false);
   const [selectedDate, setSelectedDate] = useState("");
+
   const {
     handleSubmit,
     watch,
@@ -53,60 +54,64 @@ function PersonalData() {
           <p> Gmail</p>
         </div>
       </div>
-      <Input
-        id="name"
-        placeholder={"Имя"}
-        name="name"
-        register={register}
-        errors={errors}
-        labelText="Имя"
-        required
-      />
-      <Input
-        id="surname"
-        placeholder={"Фамилия"}
-        name="surname"
-        register={register}
-        errors={errors}
-        labelText="Фамилия"
-        required
-      />
-      <Input
-        id="username"
-        placeholder={"Telegram user name"}
-        name="username"
-        register={register}
-        errors={errors}
-        labelText="Telegram user name"
-        required
-      />
-      <InputMask
-        id="phone"
-        mask={`+\\9\\9\\8 99 999 99 99`}
-        placeholder={"Введите номер"}
-        control={control}
-        name="phone"
-        labelText="Номер телефона"
-        errors={errors}
-        required
-      />
-      <InputMask
-        id="subPhone"
-        mask={`+\\9\\9\\8 99 999 99 99`}
-        placeholder={"Номер телефона (Доп)"}
-        control={control}
-        name="subPhone"
-        labelText="Номер телефона"
-        errors={errors}
-      />
-      <DatePicker
-        labelText="Дата рождения"
-        selectedDate={selectedDate}
-        open={open}
-        setOpen={setOpen}
-        handleChange={handleChange}
-      />
-      <MainButton type="submit" className={cls.btn}>
+
+      <div className={cls.inputs}>
+        <Input
+          id='name'
+          placeholder={"Имя"}
+          name='name'
+          register={register}
+          errors={errors}
+          labelText='Имя'
+          required
+        />
+        <Input
+          id='surname'
+          placeholder={"Фамилия"}
+          name='surname'
+          register={register}
+          errors={errors}
+          labelText='Фамилия'
+          required
+        />
+        <Input
+          id='username'
+          placeholder={"Telegram user name"}
+          name='username'
+          register={register}
+          errors={errors}
+          labelText='Telegram user name'
+          required
+        />
+        <InputMask
+          id='phone'
+          mask={`+\\9\\9\\8 99 999 99 99`}
+          placeholder={"Введите номер"}
+          control={control}
+          name='phone'
+          labelText='Номер телефона'
+          errors={errors}
+          required
+        />
+        <InputMask
+          id='subPhone'
+          mask={`+\\9\\9\\8 99 999 99 99`}
+          placeholder={"Номер телефона (Доп)"}
+          control={control}
+          name='subPhone'
+          labelText='Номер телефона'
+          errors={errors}
+        />
+        <DatePicker
+          labelText='Дата рождения'
+          selectedDate={selectedDate}
+          open={open}
+          setOpen={setOpen}
+          handleChange={handleChange}
+        />
+      </div>
+
+      <MainButton type='submit' className={cls.btn}>
         Сохранить изменения
       </MainButton>
     </form>
